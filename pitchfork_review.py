8b698d9efd662b00dfd09bf82efdc30ccbbd97a0
Bryan Brussee <bryanbrussee17@gmail.com>
2017-09-13 14:48:17 -0400

    stores multiple genres in a list

--- pitchfork_review.py
+++ pitchfork_review.py
@@ -3,27 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 
 # grab the page
-page = requests.get("https://pitchfork.com/reviews/albums/tori-amos-native-invader/")
+page = requests.get("https://pitchfork.com/reviews/albums/sparks-hippopotamus/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # grab the html elements
 score_raw = soup.select("span.score")
 album_raw = soup.select("h1.review-title")
 artist_raw = soup.select("ul.artist-list li  a")
-genre_raw = soup.select("a.genre-list__link")
+genre_list = soup.find_all(class_="genre-list__link")
 
 # clean html elements
 score = score_raw[0].get_text()
 album = album_raw[0].get_text()
 artist = artist_raw[0].get_text()
-genre = genre_raw[0].get_text()
+genre = []
+i = 0
+while i < len(genre_list):
+	genre.append((str(genre_list[i].get_text())))
+	i += 1
 
 # print results
 print(artist)
 print(album)
 print(score)
 print(genre)
-
 
 
 
