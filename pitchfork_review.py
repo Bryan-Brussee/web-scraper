5e75ea10fba78646813914bb07766429eb1632b6
Bryan Brussee <bryanbrussee17@gmail.com>
2017-09-13 21:10:16 -0400

    returns scraped list

--- pitchfork_review.py
+++ pitchfork_review.py
@@ -1,24 +1,34 @@
 # import libraries
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 # grab the page
-page = requests.get("https://pitchfork.com/reviews/albums/tori-amos-native-invader/")
+page = requests.get("https://pitchfork.com/reviews/albums/mount-kimbie-love-what-survives/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # grab the html elements
 score_raw = soup.select("span.score")
+bnm_raw = soup.select("p.bnm-txt")
 album_raw = soup.select("h1.review-title")
 artist_raw = soup.select("ul.artist-list li  a")
 genre_list = soup.find_all(class_="genre-list__link")
 author_raw = soup.select("a.authors-detail__display-name")
+author_detail_raw = soup.select("span.authors-detail__title")
 date_raw = soup.select("time.pub-date")
 
 # get text from elements
-score = score_raw[0].get_text()
-album = album_raw[0].get_text()
-artist = artist_raw[0].get_text()
-author = author_raw[0].get_text()
+score = float(score_raw[0].get_text())
+album = str(album_raw[0].get_text())
+artist = str(artist_raw[0].get_text())
+author = str(author_raw[0].get_text())
+author_detail = str(author_detail_raw[0].get_text())
+
+# check best new music status
+if len(bnm_raw) > 0:
+	bnm = str(bnm_raw[0].get_text())
+else:
+	bnm = "none"
 
 # loop through genres to build a list
 genre = []
@@ -29,21 +39,13 @@ while i < len(genre_list):
 
 # get the date attribute from the time tag
 time_tag = date_raw[0]
-date = time_tag['title']
-
-
-
+date = str(time_tag['title'])
 
 # print results
-print(artist)
-print(album)
-print(score)
-print(genre)
-print(author)
-print(date)
+print(artist, album, genre, score, bnm, author, author_detail, date)
 
 
-
+# goes back to page 1608
 
 
 
