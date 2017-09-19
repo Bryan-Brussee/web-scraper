16d40b89b3bdd8468cade8e5ccd257a9efd20c6e
Bryan Brussee <bryanbrussee17@gmail.com>
2017-09-18 19:48:15 -0500

    bug fixes

--- get_database.py
+++ get_database.py
@@ -5,18 +5,26 @@ from scrape_page import scrapePage
 from write_review_links import getReviewLinks
 
 
-def getDatabase(start, end):
+def getDatabase(start, end, operation):
 
 	review_links = getReviewLinks(start, end)
 
-	with open("data.csv", "wb") as f:
+	with open("data.csv", operation) as f:
 	        writeFile = csv.writer(f, dialect="excel")
-	        writeFile.writerow(["Album", "Artist", "Genre", "Score", "Best New Music", "Reviewer", "Reviewer Detail", "Date"])
+	        # writeFile.writerow(["Album", "Artist", "Genre", "Score", "Best New Music", "Reviewer", "Reviewer Detail", "Date", "URL"])
 	        i = 0
 	        while i < len(review_links):
 	        	writeFile.writerows(scrapePage(review_links[i]))
 	        	print("Scraping reviews: " + str(i + 1) + " of " + str(len(review_links)))
 	        	i += 1
+	f.close()
 
-getDatabase(39, 40)
 
+
+
+i = 131
+while i <= 1500:
+	getDatabase(i, i, "a")
+	i += 1
+
+# 130 pages of links
--- scrape_page.py
+++ scrape_page.py
@@ -17,6 +17,7 @@ def scrapePage(url):
 
 	# grab the html elements
 	artist_raw = soup.select("ul.artist-list li")
+	artist_raw = soup.find_all(class_="artists")
 	albums_raw = soup.find_all(class_="review-title")
 	genre_list = soup.find_all(class_="genre-list__link")
 	scores_raw = soup.find_all(class_="score")
@@ -25,39 +26,62 @@ def scrapePage(url):
 	author_detail_raw = soup.select("span.authors-detail__title")
 	date_raw = soup.select("time.pub-date")
 
-
-	# artist string
-	artist = artist_raw[0].get_text()
-
 	# album list
 	albums = []
+	artist = []
 	scores = []
 	bnm_list = []
 	genre = []
 	data = []
 
 
+	# # artist string
+	# if (len(artist_raw) > 1) and (artist_raw[0].get_text() != artist_raw[1].get_text()):
+	# 	i = 0
+	# 	while i < len(artist_raw):
+	# 		artist.append(artist_raw[i].get_text())
+	# 		i += 1
+	# 	artist = ', '.join(artist)
+	# else: 
+	# 	artist = artist_raw[0].get_text()
+
+
+
+	# xxx = " ".join(name)
+
+
+	# get album data
 	i = 0
 	while i < len(albums_raw):
+
+		# jump through some hoops to make sure artist names come in right
+		artist_a = artist_raw[i].find_all('a')
+		name = [a.get_text() for a in artist_a]
+		testtest = "/".join(name)
+		artist.append(testtest)
+
+		# fairly straight forward gathering of album names and scores
 		albums.append(albums_raw[i].get_text())
 		scores.append(float(scores_raw[i].get_text()))
-		# bnm_list.append(bnm_raw[i]["class"][1])
+
+		# tag best new music and best new reissues
 		if bnm_raw[i]["class"][1] == "bnm":
 			status = soup.find_all(class_=("bnm-txt"))
-			bnm_list.append(status[i].get_text())
+			bnm_list.append(status.pop(0).get_text())
 		else:
 			bnm_list.append("")
 		i += 1
 
+
 	# build genre list
 	i = 0
 	while i < len(genre_list):
-		genre.append(str(genre_list[i].get_text()))
+		genre.append(genre_list[i].get_text())
 		i += 1
+	genre = ", ".join(genre)
 
 	# pull author data
 	author = author_raw[0].get_text()
-
 	if len(author_detail_raw) > 0:
 		author_detail = author_detail_raw[0].get_text()
 	else:
@@ -71,13 +95,11 @@ def scrapePage(url):
 	data = []
 	i = 0
 	while i < len(albums):
-		data.append([albums[i], artist, genre, scores[i], bnm_list[i], author, author_detail, date])
+		data.append([albums[i], artist[i], genre, scores[i], bnm_list[i], author, author_detail, date, url])
 		i += 1
 
 	return(data)
 
-
-
-# test = scrapePage("/reviews/albums/23225-howsla/")
-# print(test)
+test = scrapePage("/reviews/albums/21899-999/")
+print(test)
 
